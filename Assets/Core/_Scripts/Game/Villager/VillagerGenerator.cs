b2b1865dda582a352e65375f219cc12fc5604501
Juan BOUILLON <herocreepyfr@gmail.com>
2024-10-11 11:14:21 +0400

    Added method to generate name from parents

--- Assets/Core/_Scripts/Game/Villager/VillagerGenerator.cs
+++ Assets/Core/_Scripts/Game/Villager/VillagerGenerator.cs
@@ -1,3 +1,4 @@
+using System;
 using UnityEngine;
 using static VillagerData;
 
@@ -20,6 +21,33 @@ public class VillagerGenerator
         m_surnames = surnamesList.text.Split('\n');
     }
 
+    /// <summary>
+    /// Generates a random name for a villager based on the surnames of two parent villagers.
+    /// </summary>
+    /// <param name="parent1">The first parent villager's data. Must not be null.</param>
+    /// <param name="parent2">The second parent villager's data. Must not be null.</param>
+    /// <returns>A string representing the generated full name of the villager, composed of a random first name and a random surname derived from the parents.</returns>
+    /// <exception cref="ArgumentNullException">Thrown when either <paramref name="parent1"/> or <paramref name="parent2"/> is null.</exception>
+    public string GenerateNameFromParents(VillagerData parent1, VillagerData parent2)
+    {
+        if (parent1 == null)
+            throw new ArgumentNullException(nameof(parent1));
+
+        if (parent2 == null)
+            throw new ArgumentNullException(nameof(parent2));
+
+        var surname1 = parent1.GetName().Split(' ')[1];
+        var surname2 = parent2.GetName().Split(' ')[1];
+        var surnames = new string[] { surname1, surname2 };
+
+        var rng = GameManager.RNG;
+        string name = m_names.PickRandom(rng).Replace("\r", "");
+        string surname = surnames.PickRandom(rng);
+
+        string fullName = $"{name} {surname}";
+        return fullName;
+    }
+
     public string GenerateName()
     {
         //Give the full name and surname
@@ -31,49 +59,45 @@ public class VillagerGenerator
         return fullName;
     }
 
-    int GetRandomID(int size)
-    {
-        int id = Random.Range(0, size);
-        return id;
-    }
-
     public int GenerateAge()
     {
-        int age = Random.Range(AGE_RANGE[AgeStage.KID].min, 20);
+        var rng = GameManager.RNG;
+        int age = rng.Next(AGE_RANGE[AgeStage.KID].min, 20);
         return age;
     }
 
     public int GenerateAge(int min, int max)
     {
-        int age = Random.Range(min, max + 1);
+        var rng = GameManager.RNG;
+        int age = rng.Next(min, max + 1);
         return age;
     }
 
-    public int GenerateAgeByStage(VillagerData.AgeStage ageStage)
+    public int GenerateAgeByStage(AgeStage ageStage)
     {
-        switch (ageStage) 
+        switch (ageStage)
         {
-            case VillagerData.AgeStage.KID:
+            case AgeStage.KID:
                 return GenerateAge(AGE_RANGE[AgeStage.KID].min, AGE_RANGE[AgeStage.KID].max);
-            case VillagerData.AgeStage.ADULT:
+            case AgeStage.ADULT:
                 return GenerateAge(AGE_RANGE[AgeStage.ADULT].min, AGE_RANGE[AgeStage.ADULT].max);
-            case VillagerData.AgeStage.ELDER:
+            case AgeStage.ELDER:
                 return GenerateAge(AGE_RANGE[AgeStage.ELDER].min, 20);
         }
 
-        return VillagerData.DEFAULT_AGE;
+        return DEFAULT_AGE;
     }
 
-    public VillagerData.Gender SelectRandomGender()
+    public Gender SelectRandomGender()
     {
         var rng = GameManager.RNG;
-        return VillagerData.ALL_GENDERS.PickRandom(rng);
+        return ALL_GENDERS.PickRandom(rng);
     }
 
-    public VillagerData.Personality SelectRandomPersonality()
+    public Personality SelectRandomPersonality()
     {
         var rng = GameManager.RNG;
-        return VillagerData.ALL_PERSONALITIES.PickRandom(rng);
+        return ALL_PERSONALITIES.PickRandom(rng);
     }
 
     public string GenerateID(string name, int populationCount)
