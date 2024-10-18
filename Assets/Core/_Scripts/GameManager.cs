10635f3f1f6ac0f41f00a8dcf2992312626cba76
Luckius_ <herocreepyfr@gmail.com>
2024-10-18 08:13:40 +0400

    Update parameters list on command help to display variable names

--- Assets/Core/_Scripts/GameManager.cs
+++ Assets/Core/_Scripts/GameManager.cs
@@ -76,7 +76,7 @@ public class GameManager : Singleton<GameManager>
                 m_commandLogManager.AddLog("Commands: ", GameManager.ORANGE);
                 foreach (var element in m_commandSystem.GetCommandsList())
                 {
-                    var text = $"- {element.identifier}{element.parameters}{(string.IsNullOrEmpty(element.description) ? "" : $": {element.description}")}";
+                    var text = $"- {element.identifier} {element.parameters}{(string.IsNullOrEmpty(element.description) ? "" : $": {element.description}")}";
                     m_commandLogManager.AddLog(text, GameManager.ORANGE, format: false);
                 }
                 SoundManager.PlaySound(SoundType.ACTION_CONFIRM);
